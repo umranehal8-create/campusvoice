076a5c7efcd7a8e9ed14dfb5a772aa9282d126e7
umranehal8 <umranehal8gmail.com>
2026-06-03 15:53:16 +0530

    fix: allow all vercel domains in CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,23 @@ const express = require("express");
 const authRoutes = require("./routes/user.routes");
 const complaintRoutes = require("./routes/complaint.routes");
 const cookieParser = require("cookie-parser");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 app.use(
-    cors({ origin: ["http://localhost:5173", "https://campusvoice-ten.vercel.app"]
-, credentials: true })
+  cors({
+    origin: function (origin, callback) {
+      if (
+        !origin ||
+        origin === "http://localhost:5173" ||
+        origin.endsWith(".vercel.app")
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
 );
 
 app.use(express.json());
